3e79f4ea890789ba74756b5a95a8bad2355f744b
Yevgen Gugel <148051488+trajil@users.noreply.github.com>
2024-08-08 15:44:47 +0200

    1

--- HelloWorld/script.js
+++ HelloWorld/script.js
@@ -1,19 +1,12 @@
-function revealWord(input1, input2) {
-    let inputWords = input1.split(', ');
-    let sentenceWords = input2.split(' ');
-    
-    for (let i = 0; i < inputWords.length; i++) {
-        for (let j = 0; j < sentenceWords.length; j++) {
-            let inputWord = inputWords[i];
-            let replacedWord = sentenceWords[j];
-            
-            if (inputWord.length === replacedWord.length && replacedWord.includes('*')) {
-                sentenceWords[j] = inputWord;
-            }
-        }
-    }
-    console.log(sentenceWords.join(' '));
-    
-}
+let date = new Date();
 
-revealWord('great, learning', 'softuni is ***** place for ******** new programming languages')
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getYear();
+
+
+
+
+document.write(day)
+// document.write(month)
+// document.write(year)
